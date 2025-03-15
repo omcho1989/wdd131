f78ab4ccfb2ad6257d5883b89a5e9ff8ff4fa679
omcho1989 <purkissete21@gmail.com>
2025-03-15 16:19:41 -0600

    added red X in js

--- scripts/temples.js
+++ scripts/temples.js
@@ -10,4 +10,10 @@ const menuitems = document.querySelector('#menuitems');
 menubutton.addEventListener('click', () => {
   menuitems.classList.toggle('open');
   menubutton.classList.toggle('hidden');
+
+  if (menuitems.classList.contains('open')) {
+    menubutton.textContent = '❌';
+  } else {
+    menubutton.textContent = '☰';
+  }
 });
