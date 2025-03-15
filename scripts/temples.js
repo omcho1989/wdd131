de9224ca535f7c3ae7d3ed5c82e4b1fe7727ba67
omcho1989 <purkissete21@gmail.com>
2025-03-15 14:40:02 -0600

    Too hard

---
+++ scripts/temples.js
@@ -0,0 +1,12 @@
+const currentYear = new Date().getFullYear();
+document.querySelector("#currentyear").innerHTML = `&copy; ${currentYear}`;
+
+const lastModified = document.lastModified;
+document.querySelector("#lastmodified").textContent = lastModified;
+
+const menubutton = document.querySelector('#menubutton');
+const menuitems = document.querySelector('nav');
+
+menubutton.addEventListener('click', () => {
+  menuitems.classList.toggle('open');
+});
